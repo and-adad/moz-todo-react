2ac9f5e38a62132769dd0d93b89b336d343427d7
Andrey <Andreyadad@gmail.com>
2024-05-02 20:55:25 +0500

    Let's open main.jsx and give our component its first prop. Back in App.jsx. Change the signature of App() so that it accepts props as a parameter and log props to the console so you can inspect it. Also delete the subject const. commenting out the <h1>Hello {subject}... line. If you open your browser's developer console, you'll see a message Object { subject: Clarice }

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 
-function App() {
-  const subject = "React";
+function App(props) {
+  console.log(props);
   return (
     <>
       <header>
         {/* Hello, React :)! */}
-        <h1>Hello, {subject + ' :)'}!</h1>
+        {/* <h1>Hello, {subject + ' :)'}!</h1> */}
         {/* Hello, REACT */}
-        <h1>Hello, {subject.toUpperCase()}</h1>
+        {/* <h1>Hello, {subject.toUpperCase()}</h1> */}
         {/* Hello, 4 */}
         <h1>Hello, {2 + 2}!</h1>
       </header>
