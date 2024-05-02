213d494ed816b2bf334a1159b62eb27d7bc0077f
Andrey <Andreyadad@gmail.com>
2024-05-02 20:35:58 +0500

    App.jsx еще варианты Выражения JavaScript как контент

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <>
       <header>
-      <h1>Hello, {subject}!</h1>
+        {/* Hello, React :)! */}
+        <h1>Hello, {subject + ' :)'}!</h1>
+        {/* Hello, REACT */}
+        <h1>Hello, {subject.toUpperCase()}</h1>
+        {/* Hello, 4 */}
+        <h1>Hello, {2 + 2}!</h1>
       </header>
     </>
   );
