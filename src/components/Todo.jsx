0f4d90ade202ce91575f07766d9ed3c3c8b50088
Andrey <Andreyadad@gmail.com>
2024-05-05 14:27:23 +0500

    Toggling the <Todo /> templates Update the Cancel button in the editingTemplate With this code in place, you should be able to press the Edit and Cancel buttons in your todo items to toggle between templates

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -13,10 +13,14 @@ function Todo(props) {
         <input id={props.id} className="todo-text" type="text" />
       </div>
       <div className="btn-group">
-        <button type="button" className="btn todo-cancel">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}>
           Cancel
           <span className="visually-hidden">renaming {props.name}</span>
         </button>
+
         <button type="submit" className="btn btn__primary todo-edit">
           Save
           <span className="visually-hidden">new name for {props.name}</span>
