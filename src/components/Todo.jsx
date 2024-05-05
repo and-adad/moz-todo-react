cbf80e6a384d907992eb71c3889abd638e6e273d
Andrey <Andreyadad@gmail.com>
2024-05-05 13:59:44 +0500

    At this point, your Todo.jsx полностью этот компонент заменил из листинга

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,12 +2,12 @@ function Todo(props) {
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-      <input
-        id={props.id}
-        type="checkbox"
-        defaultChecked={props.completed}
-        onChange={() => props.toggleTaskCompleted(props.id)}
-      />
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
         <label className="todo-label" htmlFor={props.id}>
           {props.name}
         </label>
@@ -22,7 +22,6 @@ function Todo(props) {
           onClick={() => props.deleteTask(props.id)}>
           Delete <span className="visually-hidden">{props.name}</span>
         </button>
-
       </div>
     </li>
   );
