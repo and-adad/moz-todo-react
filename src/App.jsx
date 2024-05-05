d5975d54ba586c147d3573619306480a8b695d6f
Andrey <Andreyadad@gmail.com>
2024-05-05 15:57:10 +0500

    Rendering the filters This won't work yet. We've got a bit more work to do first

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,10 @@ function App(props) {
     />
   ));
 
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton key={name} name={name} />
+  ));
+
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
@@ -71,9 +75,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
