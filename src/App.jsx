57cb9d00a2a0ac35fbe9f557d93b7715677a2ff5
Andrey <Andreyadad@gmail.com>
2024-05-05 12:37:33 +0500

    counting tasks This is almost right, except that if our list ever contains a single task, the heading will still use the word tasks. We can make this a variable, too. Update the code you just added as follows

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
 
-  const headingText = `${taskList.length} tasks remaining`;
-
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   
   return (
     <div className="todoapp stack-large">
